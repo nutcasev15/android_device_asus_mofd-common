190d9253be899e5d13f0df9db453c5dc72b4aaaa
nutcasev15 <tab.andsupp@gmail.com>
2018-07-02 10:53:51 +0530

    [GPS] Include GNSS HIDL

--- mofd.mk
+++ mofd.mk
@@ -115,6 +115,9 @@ PRODUCT_PACKAGES += \
     FlipFlap
 
 # GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
 PRODUCT_COPY_FILES += \
     device/asus/mofd-common/configs/gps.conf:system/etc/gps.conf \
     device/asus/mofd-common/configs/gps.xml:system/etc/gps.xml
