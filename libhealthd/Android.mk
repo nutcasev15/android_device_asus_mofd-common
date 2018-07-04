0b0709e765c1f0bdab5d42423c87b2c4c7c03b4b
nutcasev15 <tab.andsupp@gmail.com>
2018-07-04 12:39:31 +0530

    [HEALTHD] Adapt Code to Oreo

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -19,7 +19,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_moorefield.cpp
 LOCAL_MODULE := libhealthd.moorefield
 LOCAL_C_INCLUDES := system/core/healthd/include
-LOCAL_EXPORT_C_INCLUDE_DIRS := system/core/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := system/core/include system/core/base/include
 LOCAL_CFLAGS := -Werror
+LOCAL_STATIC_LIBRARIES := libcutils
 include $(BUILD_STATIC_LIBRARY)
 
